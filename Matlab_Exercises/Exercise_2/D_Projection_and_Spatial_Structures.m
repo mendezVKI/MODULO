46a92516effa476736e55f3471e2f73a81d79db9
Davide Ninni <d.ninni93@gmail.com>
2019-08-20 17:12:02 +0200

    Update D_Projection_and_Spatial_Structures.m

--- Matlab_Exercises/Exercise_2/D_Projection_and_Spatial_Structures.m
+++ Matlab_Exercises/Exercise_2/D_Projection_and_Spatial_Structures.m
@@ -27,11 +27,13 @@ PHI_M=zeros([n_s,R]);
 SIGMA_M=zeros([R,R]);
 
  for i=1:1:R
-disp(['Completing mPOD Mode ',num2str(i)]);
-% Normalize the columns of C to get spatial modes
-PHI_M(:,i) = PHI_M_SIGMA_M(:,i)/norm(PHI_M_SIGMA_M(:,i));
-% Assign the norm as amplitude
-SIGMA_M(i,i) = norm(PHI_M_SIGMA_M(:,i));
+     
+     disp(['Completing mPOD Mode ',num2str(i)]);
+     % Normalize the columns of C to get spatial modes
+     PHI_M(:,i) = PHI_M_SIGMA_M(:,i)/norm(PHI_M_SIGMA_M(:,i));
+     % Assign the norm as amplitude
+     SIGMA_M(i,i) = norm(PHI_M_SIGMA_M(:,i));
+     
 end
 
 % Sort the amplitudes in decreasing order
@@ -50,44 +52,41 @@ HFIG=figure(1);
 HFIG.Units='normalized';
 HFIG.Position=[0.1 0.1 0.65 0.75];
 HFIG.Name='Results mPOD Analysis- Part 1';
+
 for j=1:3
-subplot(3,2,2*j-1)
-% Reconstruct the Spatial structure
-S=reshape(Phi_M(:,j),[n_x,n_y]);
-pcolor(Xg,Yg,S);
-shading interp
-daspect([1 1 1])
-ylim([-19 19])  
-  xlim([-19 19])
- set(gca, ...
-      'Fontname', 'Palatino Linotype', ...
-      'Fontsize', 16, ...
-       'Box'         , 'off'     , ...   
-        'LineWidth'   , 1         )
- % Label Information
- xlabel('$x[-]$','Interpreter','Latex')
- ylabel('$y[-]$','Interpreter','Latex')
- title(['Spatial Structure ',num2str(j)],'Interpreter','Latex')
- set(gcf,'color','white')
+    
+    subplot(3,2,2*j-1)
+    % Reconstruct the Spatial structure
+    S=reshape(Phi_M(:,j),[n_x,n_y]);
+    pcolor(Xg,Yg,S);
+    shading interp
+    daspect([1 1 1])
+    ylim([-19 19])  
+    xlim([-19 19])
+    set(gca,'Fontname','Palatino Linotype','Fontsize',16,'Box','off','LineWidth',1)
+    % Label Information
+    xlabel('$x[-]$','Interpreter','Latex')
+    ylabel('$y[-]$','Interpreter','Latex')
+    title(['Spatial Structure ',num2str(j)],'Interpreter','Latex')
+    set(gcf,'color','white')
+
 end
 
 
 
 for j=1:3
-subplot(3,2,2*j)
-plot(t,Psi_M(:,j),'linewidth',1.5)
- set(gca, ...
-      'Fontname', 'Palatino Linotype', ...
-      'Fontsize', 16, ...
-       'Box'         , 'off'     , ...   
-        'LineWidth'   , 1         )
-xlim([0 5])
- % Label Information
- xlabel('$t[-]$','Interpreter','Latex')
- ylabel('$\widehat{\psi}_{\mathcal{M}}$','Interpreter','Latex')
- title(['T. Structure ',num2str(j)],'Interpreter','Latex')
- set(gcf,'color','white')
- end
+    
+    subplot(3,2,2*j)
+    plot(t,Psi_M(:,j),'linewidth',1.5)
+    set(gca,'Fontname','Palatino Linotype','Fontsize',16,'Box','off','LineWidth',1)
+    xlim([0 5])
+    % Label Information
+    xlabel('$t[-]$','Interpreter','Latex')
+    ylabel('$\widehat{\psi}_{\mathcal{M}}$','Interpreter','Latex')
+    title(['T. Structure ',num2str(j)],'Interpreter','Latex')
+    set(gcf,'color','white')
+ 
+end
 
 print(HFIG,'Results_mPOD.png','-dpng')
 
@@ -99,11 +98,13 @@ PHI_P_SIGMA_P=D*PSI_P;
 % Initialize the output
 Phi_P=zeros([n_s,R]);
 
- for i=1:1:R
-disp(['Completing POD Mode ',num2str(i)]);
-% Normalize the columns of C to get spatial modes
-Phi_P(:,i) = PHI_P_SIGMA_P(:,i)/Sigma_P(i,i);
-% The POD does not need a normalization for the amplitude!
+for i=1:1:R
+    
+    disp(['Completing POD Mode ',num2str(i)]);
+    % Normalize the columns of C to get spatial modes
+    Phi_P(:,i) = PHI_P_SIGMA_P(:,i)/Sigma_P(i,i);
+    % The POD does not need a normalization for the amplitude!
+    
 end
 
 
@@ -113,48 +114,42 @@ HFIG.Units='normalized';
 HFIG.Position=[0.1 0.1 0.65 0.75];
 HFIG.Name='Results POD Analysis';
 for j=1:3
-subplot(3,2,2*j-1)
-% Reconstruct the Spatial structure
-S=reshape(Phi_P(:,j),[n_x,n_y]);
+    
+    subplot(3,2,2*j-1)
+    % Reconstruct the Spatial structure
+    S=reshape(Phi_P(:,j),[n_x,n_y]);
+    pcolor(Xg,Yg,S);
+    shading interp
+    daspect([1 1 1])
+    ylim([-19 19])  
+    xlim([-19 19])
+    set(gca,'Fontname','Palatino Linotype','Fontsize',16,'Box','off','LineWidth',1)
+    % Label Information
+    xlabel('$x[-]$','Interpreter','Latex')
+    ylabel('$y[-]$','Interpreter','Latex')
+    title(['Spatial Structure ',num2str(j)],'Interpreter','Latex')
+    set(gcf,'color','white')
 
-pcolor(Xg,Yg,S);
-shading interp
-daspect([1 1 1])
-ylim([-19 19])  
-  xlim([-19 19])
- set(gca, ...
-      'Fontname', 'Palatino Linotype', ...
-      'Fontsize', 16, ...
-       'Box'         , 'off'     , ...   
-        'LineWidth'   , 1         )
- % Label Information
- xlabel('$x[-]$','Interpreter','Latex')
- ylabel('$y[-]$','Interpreter','Latex')
- title(['Spatial Structure ',num2str(j)],'Interpreter','Latex')
- set(gcf,'color','white')
 end
 
 
 
 for j=1:3
+    
     subplot(3,2,2*j)
-plot(t,PSI_P(:,j),'linewidth',1.5)
- set(gca, ...
-      'Fontname', 'Palatino Linotype', ...
-      'Fontsize', 16, ...
-       'Box'         , 'off'     , ...   
-        'LineWidth'   , 1         )
-xlim([0 5])
- % Label Information
- xlabel('$t[-]$','Interpreter','Latex')
- ylabel('$\widehat{\psi}_{\mathcal{P}}$','Interpreter','Latex')
- title(['T. Structure ',num2str(j)],'Interpreter','Latex')
- set(gcf,'color','white')
+    plot(t,PSI_P(:,j),'linewidth',1.5)
+    set(gca,'Fontname','Palatino Linotype','Fontsize',16,'Box','off','LineWidth',1)
+    xlim([0 5])
+    % Label Information
+    xlabel('$t[-]$','Interpreter','Latex')
+    ylabel('$\widehat{\psi}_{\mathcal{P}}$','Interpreter','Latex')
+    title(['T. Structure ',num2str(j)],'Interpreter','Latex')
+    set(gcf,'color','white')
+
 end
 
 print(HFIG,'Results_POD.png','-dpng')
 
-return
 
 % compare this with the introduced modes... Do they look the same ??
 
