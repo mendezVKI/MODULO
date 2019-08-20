d2b7a48cc4ed07e297b7036abfc993444cd594a4
Davide Ninni <d.ninni93@gmail.com>
2019-08-20 17:11:29 +0200

    Update A_Create_D.m

--- Matlab_Exercises/Exercise_4/A_Create_D.m
+++ Matlab_Exercises/Exercise_4/A_Create_D.m
@@ -4,7 +4,7 @@
 %% Set Up and Create Data Matrix D
 
 close all; clc; clear all
- % 1 . Data Preparation
+% 1 . Data Preparation
 % If not done yet, unzip the folder provided with the TR_piv data.
 disp('Unzipping Folder TR_PIV')
 unzip('TR_PIV','TR_PIV');
@@ -35,17 +35,17 @@ GRAD_Y=diff(Y_S);
 % non-zero gradient. The other will have zero gradient only on the change.
 IND_X=find(GRAD_X~=0,1);
 IND_Y=find(GRAD_Y~=0,1);
-if IND_X>1
-n_x=IND_X;
-n_y=(nxny/(n_x));
-Xg=reshape(X_S,[n_x,n_y]);
-Yg=reshape(Y_S,[n_x,n_y]);
-elseif IND_Y>1
-n_y=IND_Y;
-n_x=(nxny/(n_y));
 
-Xg=reshape(X_S,[n_y,n_x]);
-Yg=reshape(Y_S,[n_y,n_x]);
+if IND_X>1
+    n_x=IND_X;
+    n_y=(nxny/(n_x));
+    Xg=reshape(X_S,[n_x,n_y]);
+    Yg=reshape(Y_S,[n_x,n_y]);
+elseif IND_Y>1
+    n_y=IND_Y;
+    n_x=(nxny/(n_y));
+    Xg=reshape(X_S,[n_y,n_x]);
+    Yg=reshape(Y_S,[n_y,n_x]);
 end
 
 % Reshape u and v
@@ -57,18 +57,20 @@ V_Y=DATA(:,4); % V component
 
 filename='Exercise_4.gif';
 D=zeros(n_s,n_t);
+
 for k=1:1:n_t
-% Loop over the file name
-file = [FOLDER,filesep,'Res',num2str(k,'%05.f'),'.dat'];
-% Import data
-Data=importdata(file);
-DATA=Data.data;
-% Read columns
-V_X=DATA(:,3);
-V_Y=DATA(:,4);
-D(:,k)=[V_X;V_Y]; % Assmbly the columns of D
-disp(['Reading File ',num2str(k),' of ',...
-num2str(n_t)]); % Message to follow the process
+    
+    % Loop over the file name
+    file = [FOLDER,filesep,'Res',num2str(k,'%05.f'),'.dat'];
+    % Import data
+    Data=importdata(file);
+    DATA=Data.data;
+    % Read columns
+    V_X=DATA(:,3);
+    V_Y=DATA(:,4);
+    D(:,k)=[V_X;V_Y]; % Assmbly the columns of D
+    disp(['Reading File ',num2str(k),' of ',...
+    num2str(n_t)]); % Message to follow the process
 
 end
 
@@ -82,46 +84,42 @@ HFIG=figure(11);
 HFIG.Units='normalized';
 HFIG.Position=[0.3 0.3 0.5 0.5];
 NA=150; % Number of steps included in the video
+
 for k=1:5:NA
-disp(['Animating ',num2str(k),' of ',num2str(NA)])
-% Loop over the file name
-file = [FOLDER,filesep,'Res',num2str(k,'%05.f'),'.dat'];
-% Import data
-Data=importdata(file);
-DATA=Data.data;
-% Read columns
-V_X=reshape(DATA(:,3),[n_x,n_y]);
-V_Y=reshape(DATA(:,4),[n_x,n_y]);
-
-
-pcolor(Xg,Yg,sqrt(V_X.^2+V_Y.^2));
-shading interp
-set(gca,'YDir','reverse');
-STR=streamslice(Xg,Yg,V_X,V_Y,12,'cubic');
-set(STR,'LineWidth',0.7,'Color','k');
-  ylim([10 30])  
-  xlim([-2 35.5])
-caxis([0 7])
-     set(gca, ...
-      'Fontname', 'Palatino Linotype', ...
-      'Fontsize', 16, ...
-       'Box'         , 'off'     , ...   
-        'LineWidth'   , 1         )
+    
+    disp(['Animating ',num2str(k),' of ',num2str(NA)])
+    % Loop over the file name
+    file = [FOLDER,filesep,'Res',num2str(k,'%05.f'),'.dat'];
+    % Import data
+    Data=importdata(file);
+    DATA=Data.data;
+    % Read columns
+    V_X=reshape(DATA(:,3),[n_x,n_y]);
+    V_Y=reshape(DATA(:,4),[n_x,n_y]);
+    pcolor(Xg,Yg,sqrt(V_X.^2+V_Y.^2));
+    shading interp
+    set(gca,'YDir','reverse');
+    STR=streamslice(Xg,Yg,V_X,V_Y,12,'cubic');
+    set(STR,'LineWidth',0.7,'Color','k');
+    ylim([10 30])  
+    xlim([-2 35.5])
+    caxis([0 7])
+    set(gca,'Fontname','Palatino Linotype','Fontsize',16,'Box','off','LineWidth',1)
     % Label Information
-     xlabel('$x[mm]$','Interpreter','Latex','fontsize',18)
+    xlabel('$x[mm]$','Interpreter','Latex','fontsize',18)
     ylabel('$y[mm]$','Interpreter','Latex','fontsize',18)
-   set(gcf,'color','w')
+    set(gcf,'color','w')
 
 
- frame = getframe(11);
- im = frame2im(frame);
-       [imind,cm] = rgb2ind(im,256);
+    frame = getframe(11);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
 
-      if k == 1
-          imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
-      else
-          imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime', 0.1);
-      end
+    if k == 1
+       imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+    else
+       imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime', 0.1);
+    end
 
 end
 
