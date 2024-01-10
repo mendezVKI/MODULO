bf395665c7d4ec5c58c6e57d43ced0b5394cfcce
lorenzoschena <lorenzo.schena@vub.be>
2024-01-10 11:00:35 -0700

    missing import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,6 @@ sys.path.insert(0, os.path.abspath('../../modulo'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
     "sphinx.ext.napoleon",
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -107,6 +106,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
