b386f2c00300a9db9b63cb57eda6c56ea5455b96
lorenzoschena <lorenzo.schena@vub.be>
2024-01-10 12:55:18 -0700

    prepare to merge

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,8 @@
+import sys
+import os
 import sphinx_rtd_theme
-#
-# sys.path.insert(0, os.path.('../../modulo'))
+
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
