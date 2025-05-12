a6865ca113f32ddeaa630421e01b56997a311527
lorenzoschena <63848406+lorenzoschena@users.noreply.github.com>
2025-05-12 10:26:19 +0200

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Allow Sphinx to find the project module
-sys.path.insert(0, os.path.abspath('../../'))
+# sys.path.insert(0, os.path.abspath('../../'))
 
 import sphinx_rtd_theme
 
